e9ebbd1aac8cfa8008b30e66af60bd7ef0c1d580
BongaConnection <dlokovu100@gmail.com>
2023-03-26 21:43:17 +0200

    Changes

--- packages/frontend/utils/config.ts
+++ packages/frontend/utils/config.ts
@@ -1,5 +1,5 @@
-const API_PRODUCTION_URL = 'https://api.getorca.org';
-const API_DEV_URL = 'http://localhost:4000';
+const API_PRODUCTION_URL = 'https://test1-npgw.onrender.com/';
+const API_DEV_URL = 'https://test1-npgw.onrender.com/';
 
 export const Config = {
   API_URL: process.env.NODE_ENV === 'production' ? API_PRODUCTION_URL : API_DEV_URL,
